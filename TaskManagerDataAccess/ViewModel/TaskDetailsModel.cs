5e1072ae7de09b025bee925987b76eb11ccdba5f
ctsfds123 <43678676+ctsfds123@users.noreply.github.com>
2018-12-04 20:25:48 +0530

    Update TaskDetailsModel.cs

--- TaskManagerDataAccess/ViewModel/TaskDetailsModel.cs
+++ TaskManagerDataAccess/ViewModel/TaskDetailsModel.cs
@@ -6,22 +6,22 @@ namespace TaskManagerService.ViewModel
     public class TaskDetailsModel
     {
         [DataMember]
-        public int TaskId { get; set; } = 0;
+        public int TaskId { get; set; } 
         [DataMember]
         public string TaskName { get; set; }
         [DataMember]
-        public int ParentTaskId { get; set; } = 0;
+        public int ParentTaskId { get; set; } 
         [DataMember]
         public string ParentTaskName { get; set; }
         [DataMember]
-        public int Priority { get; set; } = 0;
+        public int Priority { get; set; } 
         [DataMember]
-        public int PriorityTo { get; set; } = 0;
+        public int PriorityTo { get; set; } 
         [DataMember]
         public DateTime StartDate { get; set; }
         [DataMember]
         public DateTime EndDate { get; set; }
         [DataMember]//(IsRequired = false)
-        public bool IsEnded { get; set; } = false;
+        public bool IsEnded { get; set; } 
     }
 }
