adf9aa3fbfea71f4aa711e459256888b1f9b42d9
rajkumar.selvaraj <rajkumar.selvaraj6@cognizant.com>
2018-12-04 19:58:17 +0530

    Api Service

---
+++ TaskManagerLoadTesting/NBenchLoadTest.cs
@@ -0,0 +1,91 @@
+﻿using NBench.Util;
+using NBench;
+using TaskManagerService.Controllers;
+using System;
+using TaskManagerService.ViewModel;
+
+namespace TaskManagerLoadTesting
+{
+    public class NBenchLoadTest
+    {
+        private TaskManagerController _ctrl;
+        private int index = 10;
+
+        [PerfSetup]
+        public void Setup(BenchmarkContext context)
+        {
+            _ctrl = new TaskManagerController();
+        }
+
+        [PerfBenchmark(Description = "Test to ensure that a minimal throughput test can be rapidly executed.",
+            NumberOfIterations = 3, RunMode = RunMode.Throughput,
+            RunTimeMilliseconds = 1000, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion("TestAddTask", MustBe.LessThan, 10000000.0d)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        [GcTotalAssertion(GcMetric.TotalCollections, GcGeneration.Gen2, MustBe.ExactlyEqualTo, 0.0d)]
+        public void BenchmarkAddTask()
+        {
+            TaskDetailsModel task = new TaskDetailsModel();
+            task.TaskName = "Task" + index.ToString();
+            task.StartDate = DateTime.Now;
+            task.EndDate = DateTime.Now.AddDays(15);
+            task.IsEnded = false;
+            task.ParentTaskName = "Parent Task" + index.ToString();
+            task.Priority = 1;
+            index++;
+            _ctrl.AddTask(task);
+        }
+
+        [PerfBenchmark(Description = "Test to ensure that a minimal throughput test can be rapidly executed.",
+           NumberOfIterations = 3, RunMode = RunMode.Throughput,
+           RunTimeMilliseconds = 1000, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion("TestViewTask", MustBe.LessThan, 10000000.0d)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        [GcTotalAssertion(GcMetric.TotalCollections, GcGeneration.Gen2, MustBe.ExactlyEqualTo, 0.0d)]
+        public void BenchmarkViewTask()
+        {
+            TaskDetailsModel task = new TaskDetailsModel();
+              
+            _ctrl.ViewTask(task);
+        }
+
+        [PerfBenchmark(Description = "Test to ensure that a minimal throughput test can be rapidly executed.",
+           NumberOfIterations = 3, RunMode = RunMode.Throughput,
+           RunTimeMilliseconds = 1000, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion("TestUpdateTask", MustBe.LessThan, 10000000.0d)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        [GcTotalAssertion(GcMetric.TotalCollections, GcGeneration.Gen2, MustBe.ExactlyEqualTo, 0.0d)]
+        public void BenchmarkUpdateTask()
+        {
+            TaskDetailsModel task = new TaskDetailsModel();
+
+            task.TaskId = 2;
+            task.TaskName = "Task2" + index.ToString();
+            task.StartDate = DateTime.Now;
+            task.EndDate = DateTime.Now.AddDays(30);
+            task.ParentTaskName = "Parent Task2" + index.ToString();
+            task.Priority = 1;
+            index++;
+            _ctrl.UpdateTask(task);
+        }
+
+        [PerfBenchmark(Description = "Test to ensure that a minimal throughput test can be rapidly executed.",
+           NumberOfIterations = 3, RunMode = RunMode.Throughput,
+           RunTimeMilliseconds = 1000, TestMode = TestMode.Test)]
+        [CounterThroughputAssertion("TestEndTask", MustBe.LessThan, 10000000.0d)]
+        [MemoryMeasurement(MemoryMetric.TotalBytesAllocated)]
+        [GcTotalAssertion(GcMetric.TotalCollections, GcGeneration.Gen2, MustBe.ExactlyEqualTo, 0.0d)]
+        public void BenchmarkEndTask()
+        {         
+
+            _ctrl.EndTask(index);
+            index++;
+        }
+
+        [PerfCleanup]
+        public void Cleanup()
+        {
+            // does nothing
+        }
+    }
+}
---
+++ TaskManagerTesting/TaskManagerTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TaskManagerService.Controllers;
+using TaskManagerService.ViewModel;
+
+namespace TaskManagerTesting
+{
+    [TestClass]
+    public class TaskManagerTest
+    {
+        [TestMethod]
+        public void Test_AddTask()
+        {
+            TaskManagerController ctrl = new TaskManagerController();
+
+            TaskDetailsModel task = new TaskDetailsModel();
+            task.TaskName = "Task2";
+            task.StartDate = DateTime.Now;
+            task.EndDate = DateTime.Now.AddDays(15);
+            task.IsEnded = false;
+            task.ParentTaskName = "Parent Task2";
+            task.Priority = 1;
+
+            ctrl.AddTask(task);
+        }
+        [TestMethod]
+        public void Test_ViewTask()
+        {
+            TaskManagerController ctrl = new TaskManagerController();
+            TaskDetailsModel task = new TaskDetailsModel();
+            ctrl.ViewTask(task);
+        }
+        [TestMethod]
+        public void Test_UpdateTask()
+        {
+            TaskManagerController ctrl = new TaskManagerController();
+
+            TaskDetailsModel task = new TaskDetailsModel();
+            task.TaskId = 2;
+            task.TaskName = "Task2";
+            task.StartDate = DateTime.Now;
+            task.EndDate = DateTime.Now.AddDays(30);
+            task.ParentTaskName = "Parent Task5";
+            task.Priority = 1;
+
+            ctrl.UpdateTask(task);
+        }
+        [TestMethod]
+        public void Test_EndTask()
+        {
+            TaskManagerController ctrl = new TaskManagerController();
+
+            int taskid = 1;
+
+            ctrl.EndTask(taskid);
+        }
+    }
+}
